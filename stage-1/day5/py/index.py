9dc14090d1b389f7722506aa43c66d3d11d142bd
Olen <lawrencenoman@gmail.com>
2024-09-06 21:21:58 +0700

    Edit file index.py

--- stage-1/day5/py/index.py
+++ stage-1/day5/py/index.py
@@ -1,7 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def helloworld():
-    return "Hello World"
+    return "Hello World! PYTHON YEAAAYYYY"
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
